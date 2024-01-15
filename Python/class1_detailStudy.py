a14036783375ad22e5444520812d5bedd5fddce9
Muhammad Zohaib Siddique <zohaibbinsabir@gmail.com>
2024-01-15 16:54:32 +0500

    class 1 modified

---
+++ Python/class1_detailStudy.py
@@ -0,0 +1,137 @@
+"""
+Difference between interpreter and compiler
+
+->  Interpreter:    1. Line by line execution
+                    2. If there is any error, it will stop execution and show the error
+->  Compiler:   1. Compilation of whole program
+                2. If there is any error, it will show all the errors at once
+
+->  for example:    1. Python is an interpreter language
+                    2. C is a compiler language
+                    3. Java is both compiler and interpreter language
+                    4. C++ is both compiler and interpreter language
+
+            
+->  Python is a high level language
+->  C is a middle level language
+->  Assembly is a low level language
+
+->  Python is a dynamically typed language
+->  C is a statically typed language
+
+->  Python is a platform independent language
+->  C is a platform dependent language
+
+->  Python is a general purpose language
+->  C is a system programming language
+
+->  Python is a scripting language
+->  C is a programming language
+
+->  Python is a functional programming language
+->  C is a procedural programming language
+
+->  Python is a object oriented programming language
+->  C is a object oriented programming language
+
+
+"""
+
+
+
+
+
+
+
+
+
+
+"""
+How Store Data in Python
+we use variables to store data in python
+There are 4 types of variables in python
+1.  int
+2.  float
+3.  string
+4.  boolean
+
+->  int:    1.  int is used to store integer values
+            2.  for example:    1
+                                2
+                                3
+                                4
+                                5
+                                6
+                                7
+                                8
+                                9
+                                10
+
+->  float:  1.  float is used to store decimal values
+            2.  for example:    1.1
+                                2.2
+                                3.3
+                                4.4
+                                5.5
+                                6.6
+                                7.7
+                                8.8
+                                9.9
+                                10.10
+
+->  string: 1.  string is used to store text
+            2.  for example:    "Hello World"
+                                "Hello Python"
+                                "Hello Class"
+                                "Hello Students"
+
+->  boolean:    1.  boolean is used to store true or false
+                2.  for example:    True
+                                    False
+
+->  Rules for naming variables:
+    1.  variable name should start with a letter or underscore
+    2.  variable name should not start with a number
+    3.  variable name can only contain alpha numeric characters and underscores (A-z, 0-9, and _ )
+    4.  variable names are case sensitive (age, Age and AGE are three different variables)
+    5.  variable name should not contain spaces
+    6.  variable name should not be a keyword
+    7.  variable name should be descriptive
+
+
+"""
+
+
+
+
+
+
+
+
+
+
+"""
+Display output on console:
+print("Hello World")
+
+Taking input from user and storing it in a variable:
+name = input("Enter your name: ")
+
+user input is always in string format, if you want to convert it into integer or float, you can do it like this:
+
+Taking input from user and converting it into integer:
+age = int(input("Enter your age: "))
+
+Taking input from user and converting it into float:
+salary = float(input("Enter your salary: "))
+
+Taking input from user and converting it into boolean:
+isMarried = bool(input("Are you married? "))
+
+check the type of variable:
+print(type(name))
+
+Taking input, converting into integer and storing into veriable in one step:
+age = int(input("Enter your age: "))
+
+"""
